162151db270f893a9689d8568ffd9fe725323029
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2024-01-29 13:13:34 +0000

    submitted degrees

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -24,7 +24,7 @@ def load_data(directory):
             people[row["id"]] = {
                 "name": row["name"],
                 "birth": row["birth"],
-                "movies": set()
+                "movies": set(),
             }
             if row["name"].lower() not in names:
                 names[row["name"].lower()] = {row["id"]}
@@ -38,7 +38,7 @@ def load_data(directory):
             movies[row["id"]] = {
                 "title": row["title"],
                 "year": row["year"],
-                "stars": set()
+                "stars": set(),
             }
 
     # Load stars
@@ -62,10 +62,17 @@ def main():
     load_data(directory)
     print("Data loaded.")
 
-    source = person_id_for_name(input("Name: "))
+    # # DEBUG REMOVE ME ///////////////////////////
+    # source = person_id_for_name("kevin bacon")
+    # # ///////////////////////////////////////////
+    source = person_id_for_name(input("Name: "))  # UNCOMMENT ME
     if source is None:
         sys.exit("Person not found.")
-    target = person_id_for_name(input("Name: "))
+
+    # # DEBUG REMOVE ME ///////////////////////////
+    # target = person_id_for_name("tom hanks")
+    # # ///////////////////////////////////////////
+    target = person_id_for_name(input("Name: "))  # UNCOMMENT ME
     if target is None:
         sys.exit("Person not found.")
 
@@ -84,19 +91,57 @@ def main():
             print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
 
-def shortest_path(source, target):
+# TODO implement breadth first search
+def shortest_path(source: str, target: str) -> list[tuple[str, str]]:
     """
     Returns the shortest list of (movie_id, person_id) pairs
     that connect the source to the target.
 
     If no possible path, returns None.
+
+    :arg: source = id string
+    :arg: target = id string
+
+    NOTE: node.state = .actor, .parent = .previous, .action = .movie
     """
 
-    # TODO
-    raise NotImplementedError
+    shortest_path: list[tuple[str, str]] = []
+
+    start = Node(actor=source, previous=None, movie=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    explored = set()
+
+    while True:
+        # IF FRONTIER IS EMPTY, RETURN NO SOLUTION
+        if frontier.empty():
+            print("No connection found.")
+            return None
+
+        # REMOVE A NODE FROM THE FRONTIER
+        node = frontier.remove()
+
+        # IF NODE CONTAINS GOAL STATE, RETURN THE SOLUTION
+        if node.actor == target:
+            while node.previous:
+                shortest_path.append((node.movie, node.actor))
+                node = node.previous
+            shortest_path.reverse()
+            return shortest_path
+
+        # ADD THE NODE TO THE EXPLORED SET
+        explored.add(node.actor)
+
+        # EXPAND NODE: ADD RESULTING NODES TO THE FRONTIER IF NOT ALREADY
+        # IN THE FRONTIER OR EXPLORED SET
+        for movie, actor in neighbors_for_person(node.actor):
+            if not frontier.contains(actor) and actor not in explored:
+                new_node = Node(actor=actor, previous=node, movie=movie)
+                frontier.add(new_node)
 
 
-def person_id_for_name(name):
+def person_id_for_name(name) -> str:
     """
     Returns the IMDB id for a person's name,
     resolving ambiguities as needed.
@@ -122,7 +167,7 @@ def person_id_for_name(name):
         return person_ids[0]
 
 
-def neighbors_for_person(person_id):
+def neighbors_for_person(person_id) -> set:
     """
     Returns (movie_id, person_id) pairs for people
     who starred with a given person.
--- degrees/util.py
+++ degrees/util.py
@@ -1,19 +1,22 @@
-class Node():
-    def __init__(self, state, parent, action):
-        self.state = state
-        self.parent = parent
-        self.action = action
+class Node:
+    def __init__(self, actor, previous, movie):
+        self.actor = actor
+        self.previous = previous
+        self.movie = movie
+        # self.state = state
+        # self.parent = parent
+        # self.action = action
 
 
-class StackFrontier():
+class StackFrontier:
     def __init__(self):
         self.frontier = []
 
     def add(self, node):
         self.frontier.append(node)
 
-    def contains_state(self, state):
-        return any(node.state == state for node in self.frontier)
+    def contains(self, actor):
+        return any(node.actor == actor for node in self.frontier)
 
     def empty(self):
         return len(self.frontier) == 0
@@ -28,7 +31,6 @@ class StackFrontier():
 
 
 class QueueFrontier(StackFrontier):
-
     def remove(self):
         if self.empty():
             raise Exception("empty frontier")
